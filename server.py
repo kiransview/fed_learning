d9ee07a6a6bb4f193428a498897c94945c1317b5
ShashiKiran <c.shashikiran421@gmail.com>
2023-11-22 00:09:12 +0530

    executable version

--- server.py
+++ server.py
@@ -1,9 +1,23 @@
 import flwr as fl
 
-# Start Flower server for three rounds of federated learning
-if __name__ == "__main__":
-    strategy = fl.server.strategy.FedAvg(
-        fraction_fit=0.1,
-        min_available_clients=2
-)
-fl.server.start_server("[::]:8080", config={"num_rounds": 3}, strategy=strategy)
+fl.server.start_server(
+    server_address = "0.0.0.0:8080", 
+    config=fl.server.ServerConfig(num_rounds=3))
+
+
+# import flwr as fl
+
+# # Start Flower server for three rounds of federated learning
+# if __name__ == "__main__":
+#     strategy = fl.server.strategy.FedAvg(
+#         fraction_fit=0.1,
+#         min_available_clients=2
+#     )
+    
+#     # Create a Config object
+#     config = fl.server.config.Config(num_rounds=3)
+
+#     fl.server.start_server(config=config, strategy=strategy)
+
+
+    ###"[::]:8080",
